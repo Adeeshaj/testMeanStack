8beaef18adfb4e6ff2b02ecbaa79449a5443b4c6
Adeeshaj <adeesha.14@cse.mrt.ac.lk>
2017-02-09 15:07:30 +0530

    connect with mongo db
    intiate the connection with database in mlab sandbox

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://adeeshaj:1234@ds147079.mlab.com:47079/mytasklist_test',['tasks']);
 
 router.get('/tasks',function (req, res , next) {
-    res.render('index.html');
+    db.tasks.find(function (err, tasks) {
+       if(err){
+           res.send(err);
+       }
+       res.json(tasks);
+    })
 });
 
 module.exports = router;
