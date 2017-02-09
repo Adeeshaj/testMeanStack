d531d6fba967e3f889bde3be846c3c9eb91398db
Adeeshaj <adeesha.14@cse.mrt.ac.lk>
2017-02-09 15:17:19 +0530

    Revert "connect with mongo db"
    This reverts commit 8beaef18adfb4e6ff2b02ecbaa79449a5443b4c6.

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var mongojs = require('mongojs');
-var db = mongojs('mongodb://adeeshaj:1234@ds147079.mlab.com:47079/mytasklist_test',['tasks']);
 
 router.get('/tasks',function (req, res , next) {
-    db.tasks.find(function (err, tasks) {
-       if(err){
-           res.send(err);
-       }
-       res.json(tasks);
-    })
+    res.render('index.html');
 });
 
 module.exports = router;
