a51aec9e46dca0c24ac9bf09baf76133bef8c8ad
Adeeshaj <adeesha.14@cse.mrt.ac.lk>
2017-02-09 15:23:14 +0530

    Revert "single task display"
    This reverts commit 8561d444c8813e10d3b53a84fd3b9f5659537d63.
    
    # Conflicts:
    #	routes/tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/tasks',function (req, res , next) {
+<<<<<<< HEAD
     res.render('index.html');
+=======
+    db.tasks.find(function (err, tasks) {
+       if(err){
+           res.send(err);
+       }
+       res.json(tasks);
+    })
+>>>>>>> parent of 8561d44... single task display
 });
 
 module.exports = router;
