8561d444c8813e10d3b53a84fd3b9f5659537d63
Adeeshaj <adeesha.14@cse.mrt.ac.lk>
2017-02-09 15:15:32 +0530

    single task display

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,12 +3,23 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://adeeshaj:1234@ds147079.mlab.com:47079/mytasklist_test',['tasks']);
 
+//get all tasks
 router.get('/tasks',function (req, res , next) {
     db.tasks.find(function (err, tasks) {
        if(err){
            res.send(err);
        }
        res.json(tasks);
+    })
+});
+
+//get signle tasks
+router.get('/task/:id',function (req, res , next) {
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function (err, task) {
+       if(err){
+           res.send(err);
+       }
+       res.json(task);
     })
 });
 
