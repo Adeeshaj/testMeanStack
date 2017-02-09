d466ce94994e14e8000b4cc37319d5dfd0aae752
Adeeshaj <adeesha.14@cse.mrt.ac.lk>
2017-02-09 15:28:29 +0530

    get method in rest
    get single task

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,17 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://adeeshaj:1234@ds147079.mlab.com:47079/mytasklist_test',['tasks']);
 
+//get all tasks
 router.get('/tasks',function (req, res , next) {
-<<<<<<< HEAD
+
     res.render('index.html');
-=======
+
     db.tasks.find(function (err, tasks) {
        if(err){
            res.send(err);
        }
        res.json(tasks);
     })
->>>>>>> parent of 8561d44... single task display
+
+});
+
+//get signle task
+router.get('/task/:id',function (req, res , next) {
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function (err, task) {
+       if(err){
+           res.send(err);
+       }
+       res.json(task);
+    })
 });
 
 module.exports = router;
